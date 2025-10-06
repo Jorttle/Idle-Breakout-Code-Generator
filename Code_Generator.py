8595169ba9766e3ab5bea6d2737b0ddbd35383e0
Jorttle <99557254+Jorttle@users.noreply.github.com>
2025-10-05 18:06:12 -0600

    Update Code_Generator.py
    Fixed black bricks in the wrong position

--- Code_Generator.py
+++ Code_Generator.py
@@ -1,5 +1,5 @@
 import base64
-# {level},{money},{gold},{gold_claim},0,0,0,{black_bricks},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{boss_level},{net_skill_points},0,0,0,
+# {level},{money},{gold},{gold_claim},0,0,0,{black_bricks},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{boss_level},{skill_points},0,0,0,
 
 money = input('How much money do you want?: ')
 level = input('What level do you want to be at?: ')
@@ -8,9 +8,9 @@ gold_claim = input('How much gold should you claim on reset?: ')
 black_bricks = input('How many BB (black bricks) do you want?: ')
 skill_points = input('How many skill points do you want?: ')
 
-boss_level = skill_points
+boss_level = '0'
 
-code = f'{level},{money},{gold},{gold_claim},0,0,0,{black_bricks},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{boss_level},{skill_points},0,0,0,'
+code = f'{level},{money},{gold},{gold_claim},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{black_bricks},0,0,0,{boss_level},{skill_points},0,0,0,'
 # Encode the string to bytes (UTF-8)
 code_bytes = code.encode("utf-8")
 
@@ -21,5 +21,4 @@ encoded_bytes = base64.b64encode(code_bytes)
 encoded_str = encoded_bytes.decode("utf-8")
 
 print(encoded_str)
-
 
